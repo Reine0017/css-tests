b1330bd77445edff2e937853b747c88c9dfc8c5f
Reine0017 <m.fortitudo.fr@gmail.com>
2021-01-12 23:28:45 +0900

    downgrade version

--- react-test/src/components/CoverflowEffect.js
+++ react-test/src/components/CoverflowEffect.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Swiper from 'react-id-swiper';
-import 'swiper/swiper-bundle.css'
+// import 'swiper/css/swiper.css';
 
 function CoverflowEffect(){
   const params = {
